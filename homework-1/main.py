f2803dc79e1dcd230e1fd25c3ce69fb83a67d786
dmitry <dima.trenirovki@mail.ru>
2023-07-23 22:33:29 +0300

    home_work base_date

--- homework-1/main.py
+++ homework-1/main.py
@@ -23,13 +23,41 @@ def add_data_to_customers(path = './north_data/customers_data.csv'):
         #close cursor mand connection
         conn.close()
 
+def add_data_to_employees(path = './north_data/employees_data.csv'):
+    with open(path, 'r') as file:
+        content = csv.reader(file, delimiter=',')
+        ls_content = []
+        for i in content:
+            ls_content.append(i)
+        conn = psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password='qwerty'
+)
 
+        #create cursor
+        cur = conn.cursor()
+        cur.executemany('INSERT INTO employees VALUES(%s, %s, %s, %s, %s, %s)', ls_content[1:])
+        conn.commit()
+        #close cursor mand connection
+        conn.close()
 
-#add_data_to_customers()
-
-
-
-#- Написать скрипт в `main.py`, который заполнит созданные таблицы данными из `north_data`
-#- Для подключения к БД использовать библиотеку `psycopg2`
-#- Зайти в pgAdmin и убедиться, что данные в таблицах есть
-
+def add_to_orders_data(path = './north_data/orders_data.csv'):
+    with open(path, 'r') as file:
+        content = csv.reader(file, delimiter=',')
+        ls_content = []
+        for i in content:
+            ls_content.append(i)
+        conn = psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password='qwerty'
+)
+        #create cursor
+        cur = conn.cursor()
+        cur.executemany('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', ls_content[1:])
+        conn.commit()
+        #close cursor mand connection
+        conn.close()
